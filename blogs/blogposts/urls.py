63f45b65409002f60078f007164c907a7cdabdc4
AKSHATA-22 <akshatasangwai1234@gmail.com>
2021-03-07 15:26:24 +0530

    buttons working

--- blogs/blogposts/urls.py
+++ blogs/blogposts/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path("aboutus", views.aboutus, name='aboutus'),
+    path("marketing", views.marketing, name='marketing'),
+    path("blog", views.blog, name='blog'),
+    path("contactus", views.contactus, name='contactus'),
+    # path("login", views.lo, name='login'),
 ]
--- blogs/blogposts/views.py
+++ blogs/blogposts/views.py
@@ -5,4 +5,12 @@ from django.contrib.auth.models import User, auth
 # Create your views here.
 
 def index(request):  
+    return render(request, "index.html")
+def aboutus(request):  
+    return render(request, "about.html")
+def marketing(request):  
     return render(request, "marketing.html")
+def blog(request):  
+    return render(request, "blog.html")
+def contactus(request):  
+    return render(request, "contact.html")
